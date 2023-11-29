4c8a8c2b1fe7e4045522f26732c457e2afbdfd60
mdsajalali <mdsajalali2020@gmail.com>
2023-11-29 11:38:01 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("User Management Server Running...");
+})
+
+app.listen(port, () => {
+    console.log(`User Management Server Running On ${port}`);
+})
+
+
+
